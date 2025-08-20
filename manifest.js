89076bb53bf98c77d1af2433d9be85e6ee2b6eb3
yyyeoJ <94957781+yyyeoJ@users.noreply.github.com>
2025-08-20 15:01:42 +0200

    ios standalone mode

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Snake",
+  "short_name": "Snake",
+  "icons": [
+    {
+      "src": "icon.jpg",
+      "sizes": "192x192",
+      "type": "image/jpg"
+    }
+  ],
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000"
+}
